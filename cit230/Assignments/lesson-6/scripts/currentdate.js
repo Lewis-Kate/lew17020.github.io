83e4480cc6b98078f76c37554b5a74f66ae168bc
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-02-18 18:18:30 -0500

    remove month array

--- cit230/Assignments/lesson-6/scripts/currentdate.js
+++ cit230/Assignments/lesson-6/scripts/currentdate.js
@@ -3,18 +3,6 @@ function currentDate() {
       var currentDate = new Date();
       var day = currentDate.getDate();
       var month = currentDate.getMonth();
-        month[0] = "January";
-        month[1] = "February";
-        month[2] = "March";
-        month[3] = "April";
-        month[4] = "May";
-        month[5] = "June";
-        month[6] = "July";
-        month[7] = "August";
-        month[8] = "September";
-        month[9] = "October";
-        month[10] = "November";
-        month[11] = "December";
       var year = currentDate.getFullYear();
       var d = new Date();
       var weekday = new Array(7);
@@ -25,7 +13,7 @@ function currentDate() {
         weekday[4] = "Thursday";
         weekday[5] = "Friday";
         weekday[6] = "Saturday";
-      var n = weekday[d.getDay()]+ " " + day + " " + month + " " + year;
+      var n = weekday[d.getDay()]+"," + " " + day + " " + month + " " + year;
 
       document.getElementById("currentdate").innerHTML = n;
 }
