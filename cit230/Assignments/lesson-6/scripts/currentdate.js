53c91d383ecd508646caab575db750a1774c5ec0
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-02-17 21:57:44 -0500

    javascript

---
+++ cit230/Assignments/lesson-6/scripts/currentdate.js
@@ -0,0 +1,12 @@
+
+function myFunction() {
+      var currentDate = new Date();
+      var day = currentDate.getDate();
+      var month = currentDate.getMonth() + 1;
+      var year = currentDate.getFullYear();
+  document.write(day + "/" + month + "/" + year)
+
+      document.getElementById("currentdate").innerHTML = n;
+}
+
+    
