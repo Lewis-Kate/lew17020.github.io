e79c737a2fb8e23db584463d67a7c05bf5afd47f
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-02-17 22:17:47 -0500

    full date

--- cit230/Assignments/lesson-6/scripts/currentdate.js
+++ cit230/Assignments/lesson-6/scripts/currentdate.js
@@ -16,7 +16,15 @@ function currentDate() {
         month[10] = "November";
         month[11] = "December";
       var year = currentDate.getFullYear();
-      var n =(day + " " + "month" + " " + year);
+      var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+      var n =([weekday.getDay] + " " + day + " " + [month.getMonth()] + " " + year);
 
       document.getElementById("currentdate").innerHTML = n;
 }
