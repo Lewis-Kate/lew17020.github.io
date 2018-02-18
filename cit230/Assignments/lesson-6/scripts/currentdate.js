20770f1dfd79e4085cb4db5d7346fd44b8ae9d1f
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-02-17 22:05:52 -0500

    month array

--- cit230/Assignments/lesson-6/scripts/currentdate.js
+++ cit230/Assignments/lesson-6/scripts/currentdate.js
@@ -2,7 +2,7 @@
 function currentDate() {
       var currentDate = new Date();
       var day = currentDate.getDate();
-      var month = currentDate.getMonth() + 1;
+      var month = currentDate.getMonth(month['01','January']['02','February']['03','March']['04','April']['05','May']['06','June']) + 1;
       var year = currentDate.getFullYear();
       var n =(day + "/" + month + "/" + year);
 
