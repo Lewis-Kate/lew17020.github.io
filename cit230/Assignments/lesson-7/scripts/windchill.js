32c9529db139dd6541ff0544c23f949b9d6f2c9a
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-02-22 20:06:49 -0500

    windchill

--- cit230/Assignments/lesson-7/scripts/windchill.js
+++ cit230/Assignments/lesson-7/scripts/windchill.js
@@ -1,8 +1,8 @@
 function doInputOutput() {
-		var high = parseFloat(document.getElementById("hightemp").value);
-        var low = parseFloat(document.getElementById("lowtemp").value);
-        var tempF = (high + low)/2;
-		var speed = parseFloat(document.getElementById("windspeed").value);
+		var high = 90;
+        var low = 66;
+        var tempF = (high + low) / 2;
+		var speed = 5;
 		var wind = windChill(tempF, speed);
 		document.getElementById("outputDiv").innerHTML = wind.toFixed(2);
 		}
@@ -10,3 +10,4 @@ function doInputOutput() {
 	function windChill(tempF, speed) {
 		var windchill = 35.74 + .6215 * tempF - 35.75 * Math.pow(speed, .16) +.4275 * tempF * Math.pow(speed, .16);
 		return windchill;
+    }
