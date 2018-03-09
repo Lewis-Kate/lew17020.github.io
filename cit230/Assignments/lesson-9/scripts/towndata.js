b7335aa4f22bd2107d0ec127c8e3a8881b7d1c97
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-03-09 16:58:18 -0500

    responsive design

---
+++ cit230/Assignments/lesson-9/scripts/towndata.js
@@ -0,0 +1,8 @@
+var requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  var towndata = request.response;
+}
