6dbc04b82cd6622afed84ea7f2631582962a75e9
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-03-10 13:03:24 -0500

    franklin JSON

---
+++ cit230/Assignments/lesson-9/scripts/wu-weather.js
@@ -0,0 +1,23 @@
+var weatherObject = new XMLHttpRequest();
+
+weatherObject.open('GET','http://api.wunderground.com/api/ae1f47097e29c996/forecast/geolookup/conditions/q/MN/Franklin.json',true)
+
+weatherObject.send();
+
+weatherObject.onload = function(){
+
+	var weatherInfo = JSON.parse(weatherObject.responseText);
+	console.log(weatherInfo);
+	
+	document.getElementById('temp').innerHTML = weatherInfo.current_observation.temperature_string;
+	
+	document.getElementById('conditions').innerHTML = weatherInfo.current_observation.weather;
+	
+	document.getElementById('pic').src = weatherInfo.current_observation.icon_url;
+	
+	document.getElementById('wind').innerHTML = weatherInfo.current_observation.wind_mph;
+	
+	document.getElementById('precip').innerHTML = weatherInfo.current_observation.precip_today_in;
+	
+	document.getElementById('today').innerHTML = weatherInfo.forecast.simpleforecast.forecastday[0,1];
+} // end of on load
