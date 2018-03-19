b32ebc79c97bfcb1a4f43b7aafa07e1c4dd95c08
lew17020 <35283629+lew17020@users.noreply.github.com>
2018-03-19 12:49:12 -0400

    json array

--- cit230/Assignments/lesson-9/scripts/towndata.js
+++ cit230/Assignments/lesson-9/scripts/towndata.js
@@ -1,16 +1,11 @@
 var towndata = new XMLHttpRequest();
-towndata.open('', 'https://byui-cit230.github.io/weather/data/towndata.json', true);
+towndata.open('GET', 'https://byui-cit230.github.io/weather/data/towndata.json', true);
 towndata.send();
 
+towndata.onload = function(){
 
-request.onload = function() {
-	var towndata = JSON.parse(request.responseText)['towns'];
-	
-		document.getElementById('franklin_moto').innerHTML = Town.towns["0"].motto.fcttext;
-	}
-
-	
-
+	var towns = JSON.parse(towndata.responseText);
+	console.log(towns);
 	
 	
 }
